7cd0086643cbee7b4a12cf75e3403894b5075284
Tim Verlage <92015761+Tivva34@users.noreply.github.com>
2025-03-01 17:03:56 +0100

    Update movieCard.js
    Ändrade tillbaks pga fel kod

--- fold/IMDO/template/scripts/components/movieCard.js
+++ fold/IMDO/template/scripts/components/movieCard.js
@@ -1,7 +1,39 @@
-import { saveToFavorites, removeFromFavorites } from './favorites.js';
+import { saveToFavorites } from './favorites.js';
 
 
 const missingImage = './IMDO/template/res/missing-poster.jpg';  // Backup image
+
+// movie class
+export class Movie {
+    constructor(data) {
+        this.title = data.title;
+        this.year = data.year;
+        this.imdbID = data.imdbID;
+        this.posterUrl = data.Poster;
+        this.plot = data.Plot;
+    }
+
+    // get movie details
+    getTitle() {
+        return this.title;
+    }
+
+    getYear() {
+        return this.year;
+    }
+
+    getImdbID() {
+        return this.imdbID;
+    }
+
+    getPoster() {
+        return this.posterUrl;
+    }
+
+    getPlot() {
+        return this.plot;
+    }
+}
 
 // movie card function
 export function createMovieCard(movieData) {
@@ -88,7 +120,12 @@ export async function renderFavorites() {
     });
 }
 
-
+// Remove movie from favorites
+function removeFromFavorites(imdbID) {
+    let favorites = JSON.parse(localStorage.getItem("favoriteMovies")) || [];
+    favorites = favorites.filter(movie => movie.imdbID !== imdbID); // at bort movie efter imdbID
+    localStorage.setItem("favoriteMovies", JSON.stringify(favorites));
+}
 
 
 export function updateFavoriteButtons() {
